f0e8eb4f301d7322795d7cf90399d2564da23e1e
juuliawsss <juuliawylipelkonen@gmail.com>
2025-09-09 09:56:05 +0300

    spawn

--- Assets/Scripts/FireflySpawner.cs
+++ Assets/Scripts/FireflySpawner.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class FireflySpawner : MonoBehaviour
 {
     public GameObject fireflyPrefab;
-    public float spawnInterval = 7f;
+    public float spawnInterval = 2f;
 
     private float timer = 0f;
 
@@ -19,7 +19,11 @@ public class FireflySpawner : MonoBehaviour
 
     void SpawnFirefly()
     {
-        Vector3 spawnPosition = transform.position;
+        float x = Random.Range(-70f, 70f); // Change range as needed
+        float y = transform.position.y;    // Keep y the same as spawner
+        float z = Random.Range(-70f, 70f); // Change range as needed
+
+        Vector3 spawnPosition = new Vector3(x, y, z);
         Instantiate(fireflyPrefab, spawnPosition, Quaternion.identity);
     }
 }
