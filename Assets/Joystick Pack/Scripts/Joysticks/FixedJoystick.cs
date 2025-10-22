729d3caae6e3b0046d50c6d1f5bf5d711eab9d96
juuliawsss <juuliawylipelkonen@gmail.com>
2025-10-22 08:12:30 +0300

    d

--- Assets/Joystick Pack/Scripts/Joysticks/FixedJoystick.cs
+++ Assets/Joystick Pack/Scripts/Joysticks/FixedJoystick.cs
@@ -4,5 +4,10 @@ using UnityEngine;
 
 public class FixedJoystick : Joystick
 {
-
+    protected override void HandleInput(float magnitude, Vector2 normalised, Vector2 radius, Camera cam)
+    {
+        base.HandleInput(magnitude, normalised, radius, cam);
+        // Move the handle within the joystick background
+        handle.anchoredPosition = normalised * radius * magnitude;
+    }
 }
